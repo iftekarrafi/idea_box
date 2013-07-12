b436896218ebb06ad4e9e2c0d2b0857014fc0cda
Iftekar Rafi <neoraf11@yahoo.com>
2013-07-12 10:18:41 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
+require 'sinatra/reloader'
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
-  get '/' do
-    "Hello, World!"
+  configure :development do 
+		register Sinatra::Reloader
+	end
+
+  not_found do
+    erb :error
   end
+
+  get '/' do
+     erb :index, locals: {ideas: Idea.all}
+  end
+
+  post '/' do
+  	idea = Idea.new(params['idea_title'], params['idea_description'])
+  	idea.save
+  	redirect '/'
+  end
+
 end
